e76c9f6f592d75b987fffc2cbcab513261ea8375
gregaswright <greg.a.s.wright@gmail.com>
2020-10-02 16:07:12 -0400

    finished

--- app/models/cli.rb
+++ app/models/cli.rb
@@ -2,13 +2,16 @@ require "pry"
 class CLI
 
     def start
-        puts "\n               Hello and Welcome to Recipe Roulette.\n"
+
+
+
+        puts "\n               Hello and Welcome to Globel Recipe International.\n"
         greeting
     end
 
 
     def greeting
-        puts "\n Sign-in or Sign-up?"
+        puts "\n Sign-in or Sign-up?".cyan
         puts "\n Enter 1 to Sign-in, or 2 to Sign-up"
         choice = gets.chomp
         case choice
@@ -17,7 +20,7 @@ class CLI
             when "2"
                 sign_up
             else
-                puts "Invalid Entry"
+                puts "Invalid Entry".red
                     greeting
             end
     end
@@ -30,7 +33,7 @@ class CLI
             puts "\n Please Enter Your Password"
             password = gets.chomp
             if User.exists?(password: password)
-                puts "\n Welcome to your Recipe Roulette, #{user_name}"
+                puts "\n Welcome to your Globel Recipe International, #{user_name}".green
                 @this_user = User.find_by(user_name: user_name)
                 main_menu
             else
@@ -38,7 +41,7 @@ class CLI
                 sign_in
             end
         else
-            puts "\n Invalid User Name."
+            puts "\n Invalid User Name.".red
             greeting
         end
     end
@@ -53,18 +56,18 @@ class CLI
             puts "\n Please Create Password"
             password = gets.chomp
             @this_user = User.create(user_name: user_name, password: password)
-            puts "\n You have Successfully Created an Account, Welcome to Recipe Roulette, #{user_name}"
-            user_interface
+            puts "\n You have Successfully Created an Account, Welcome to Globel Recipe International, #{user_name}".green
+            main_menu
         end
     end
 
 
     def main_menu
-        puts "\n What would you like to do?"
-        puts "\n Enter 1 to see your saved recipes?"
-        puts "\n Enter 2 to select a random set of recipes based on a category."
+        puts "\n What would you like to do?".cyan
+        puts "\n Enter 1 to see your saved recipes."
+        puts "\n Enter 2 to select a set of recipes based on a cuisine."
         puts "\n Enter 3 to add a comment to one of your existing recipes."
-        puts "\n Enter 4 to return to log-in screen."
+        puts "\n Enter 4 to log-out."
         choice = gets.chomp
         case choice
             when "1"
@@ -72,11 +75,11 @@ class CLI
             when "2"
                 recipe_category
             when "3"
-                add_a_comment
+                add_a_comment_with_user_recipes
             when "4"
-                greeting
+                start
             else
-                puts "\n Invalid Entry"
+                puts "\n Invalid Entry".red
                 main_menu
             end
     end
@@ -86,25 +89,27 @@ class CLI
         UserRecipe.all.each do |u_recipe|
             if u_recipe.user_id == @this_user.id
                x = u_recipe.recipe_id
+               y = u_recipe.comment
                @recipe = Recipe.all.find(x)
-               puts "#{@recipe.name}"
+               puts "\n"
+               puts "#{@recipe.name}                    comment: #{y}".light_blue
            
             end
         end
-        puts "\n Would you like to Delete or Add a Comment to any of these recipes?"
+        puts "\n Would you like to delete or add a comment to any of these recipes?".cyan
         puts "\n Press 1 to Delete a Recipe"
         puts "\n Press 2 to Add a Comment"
         puts "\n Press 3 to Return to Main Menu"
         choice = gets.chomp
         case choice
             when "1"
-                delete_a_recipe
+                remove_saved_recipe
             when "2"
                 add_a_comment
             when "3" 
                 main_menu
             else
-                puts "\n Invalid input, returning to Main Menu"
+                puts "\n Invalid input, returning to Main Menu".red
                 main_menu
         end
     end
@@ -112,17 +117,24 @@ class CLI
 
     #needs to return a random amount from recipe cuisines
     def recipe_category
-        puts "What type of cuisine are you in the mood for?"
+        puts "What type of cuisine are you in the mood for?".cyan
         puts "Push 1 for Italian"
         puts "Push 2 for American"
+        puts "Push 3 for Japanese"
+        puts "push 4 for Chinese"
         choice = gets.chomp
         case choice
             when "1"
                 find_a_random_recipe("Italian")
             when "2"
                 find_a_random_recipe("American")
+            when "3"
+                find_a_random_recipe("Japanese")
+            when "4"
+                find_a_random_recipe("Chinese")
             else
-                puts "\n Invalid Entry"
+                puts "\n Invalid Entry".red
+                recipe_category
         end
     end
 
@@ -131,10 +143,11 @@ class CLI
         Recipe.all.select do |recipe|
             if recipe.cuisine == recipe_cuisine
                 random_recipe = recipe.name
+                puts "\n"
                 puts "#{random_recipe}"
             end
         end
-        puts "\n Would you like to add one of these Recipes?"
+        puts "\n Would you like to add one of these recipes?".cyan
                 puts "\n Press y for Yes"
                 puts "\n Press n for No"
                 choice = gets.chomp
@@ -142,35 +155,36 @@ class CLI
                 when "y"
                     add_recipe
                 when "n"
-                    puts "\n Would you like a new random #{recipe_cuisine} recipe or a differnt cuisine?"
-                    puts "\n Press 1 for new #{recipe_cuisine} recipes."
-                    puts "\n Press 2 for a differnt cuisine."
-                    puts "\n Press 3 to return to main menu."
+                    puts "\n Would you like to see a differnt cuisine?".cyan
+                    puts "\n Press 1 for a differnt cuisine."
+                    puts "\n Press 2 to return to main menu."
                     second_choice = gets.chomp
                     case second_choice
-                    when "1"
-                        find_a_random_recipe(recipe_cuisine)
-                    when "2"
-                        recipe_category
-                    when "3"
-                        main_menu
-                    else
-                        puts "\n Invalid Entry"
-                        recipe_category
+                        when "1"
+                            recipe_category
+                        when "2"
+                            main_menu
+                        else
+                            puts "\n Invalid Entry".red
+                            recipe_category
                     end
+                else
+                    puts "\n Invalid Entry".red
+                    recipe_category
+                    find_a_random_recipe(recipe_cuisine)
                 end
     end
 
 
     def add_recipe
-        puts "\n Please type the Recipe from the List that you would like to add."
+        puts "\n Please type the recipe from the list that you would like to add.".cyan
         recipe_name = gets.chomp
             if Recipe.exists?(name: recipe_name)
                 recipe_object = Recipe.find_by(name: recipe_name)
-                UserRecipe.create(user_id: @this_user.id, recipe_id: recipe_object.id, comment: "No current comment")
-                puts "\n Great! This Recipe has been added to your Saved Recipes."
+                UserRecipe.create(user_id: @this_user.id, recipe_id: recipe_object.id, comment: "No current comment.")
+                puts "\n Great! #{recipe_name} has been added to your Saved Recipes.".green
             else
-                puts "\n Recipe not found, to try again press 1, press 2 to return to main menu."
+                puts "\n #{recipe_name} not found, to try again press 1, press 2 to return to main menu.".red
                 choice = gets.chomp
                 case choice
                 when "1"
@@ -178,28 +192,70 @@ class CLI
                 when "2"
                     main_menu
                 else
-                    puts "\n Invalid Entry"
+                    puts "\n Invalid Entry".red
                     add_recipe
                 
             end
         end
-        puts "\n Would you like to add another recipe?"
-        puts "\n Press 1 to add another recipe"
-        puts "\n Press 2 to return to main menu"
+        puts "\n Would you like to add another recipe?".cyan
+        puts "\n Press 1 to add another recipe from this list."
+        puts "\n Press 2 to choose from a differnt cuisine."
+        puts "\n Press 3 to return to main menu."
+        puts "\n"
         choice = gets.chomp
             case choice
                 when "1"
                     add_recipe
                 when "2"
+                    recipe_category
+                when "3"
                     main_menu
                 else
-                    puts "\n Invalid Entry"
+                    puts "\n Invalid Entry, returning to main menu.".red
                     main_menu
             end
     end
 
-
     def add_a_comment
+        puts "\n What Recipe would you like to add a comment on?".cyan
+        recipe_name = gets.chomp
+        recipe = Recipe.find_by(name: recipe_name)
+        user_recipe = UserRecipe.find_by(user_id: @this_user.id, recipe_id: recipe.id)
+        if !user_recipe
+            puts "\n Looks like you haven't saved this recipe, try again or return to main menu?".red
+            puts "\n Press 1 to try again."
+            puts "\n Press 2 to return to main menu."
+            puts "\n"
+            choice = gets.chomp
+            case choice
+                when "1"
+                    add_a_comment
+                when "2"
+                    main_menu
+                else
+                    puts "\n Invalid Entry.".red
+                    main_menu
+            end
+        else
+            puts "\n Please input your comment.".cyan
+            user_comment = gets.chomp
+            user_recipe.update(comment: user_comment)
+            puts "\n Great! Your Comment has been added!".green
+            puts "\n Would you like to make a comment on a differnt recipe?".cyan
+            puts "\n If yes press y, if no press any other key to return to main menu."
+            puts "\n"
+            choice_2 = gets.chomp
+            case choice_2
+                when "y"
+                    add_a_comment
+                else
+                    main_menu
+            end
+        end
+    end
+
+
+    def add_a_comment_with_user_recipes
         puts "\n"
         UserRecipe.all.each do |u_recipe|
             if u_recipe.user_id == @this_user.id
@@ -208,14 +264,14 @@ class CLI
                puts "#{@recipe.name}"
            end
         end
-        puts "\n What Recipe would you like to add a comment on?"
+        puts "\n What Recipe would you like to add a comment on?".cyan
         recipe_name = gets.chomp
         recipe = Recipe.find_by(name: recipe_name)
         user_recipe = UserRecipe.find_by(user_id: @this_user.id, recipe_id: recipe.id)
         if !user_recipe
-            puts "\n Looks like you haven't saved this recipe, try again or return to main menu?"
-            puts "\n Press 1 to try again"
-            puts "\n Press 2 to return to main menu"
+            puts "\n Looks like you haven't saved this recipe, try again or return to main menu?".red
+            puts "\n Press 1 to try again."
+            puts "\n Press 2 to return to main menu."
             choice = gets.chomp
             case choice
                 when "1"
@@ -223,15 +279,34 @@ class CLI
                 when "2"
                     main_menu
                 else
-                    puts "\n Invalid Entry"
+                    puts "\n Invalid Entry".red
                     main_menu
             end
         else
-            puts "\n Please input your comment"
+            puts "\n Please input your comment.".cyan
             user_comment = gets.chomp
             user_recipe.update(comment: user_comment)
+            puts "\n Great! Your Comment has been added!".green
+            puts "\n Would you like to make a comment on a differnt recipe?".cyan
+            puts "\n If yes press y, if no press any other key to return to main menu."
+            choice_2 = gets.chomp
+            case choice_2
+                when "y"
+                    add_a_comment
+                else
+                    main_menu
+            end
         end
+    end
 
+    def remove_saved_recipe
+        puts "\n Which recipe would you like to remove?".cyan
+        remove = gets.chomp
+            remove_recipe_id = Recipe.where(name: remove).ids
+            recipe_to_remove = UserRecipe.find_by(user_id: @this_user.id, recipe_id: remove_recipe_id)
+            UserRecipe.delete(recipe_to_remove.id)
+            puts "\n #{remove} has been deleted from your recipe list. Your list now only includes:".green
+            saved_recipes
     end
 
     
